2a855e1c7932bdb0d33c8622adda5f73ab9413c2
Mikhail Shubin <mikhail.shubin@repoint.kz>
2017-12-03 11:27:29 +0600

    missing migration

--- MrWay.Data/Migrations/20171203052714_Add_OrderIsCompleted.Designer.cs
+++ MrWay.Data/Migrations/20171203052714_Add_OrderIsCompleted.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace MrWay.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171203052714_Add_OrderIsCompleted")]
+    partial class Add_OrderIsCompleted
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
